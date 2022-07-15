274b88a3ec36c1dea8e820fa22dfa7d35ebac361
Jannik <Jannik.Hoog@gmx.de>
2022-07-15 13:00:19 +0200

    jannik

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -2,6 +2,8 @@ package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
 public class QueryProcessor {
 
@@ -14,14 +16,22 @@ public class QueryProcessor {
      */
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "jannik";
-        } else { // TODO extend the programm here
+            return "jannik";
+        } else if (query.contains("plus")) {
+            String[] arrayquery = query.split(" ");
+            for (int i = 0; i < arrayquery.length; i++) {
+                if (arrayquery[i].equals("plus")) {
+                    return "" + Integer.parseInt(arrayquery[i - 1]) + Integer.parseInt(arrayquery[i + 1]);
+                }
+            }
+            return "";
+        } else {
             return "";
         }
     }
